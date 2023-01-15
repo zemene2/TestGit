aa08bef05eeb7a736894043a48ed0f601070ef28
zemene22 <zamana2212@gmail.com>
2023-01-15 16:51:55 +0200

    asdfgh

--- src/main/java/com/example/testgit/TestGitApplication.java
+++ src/main/java/com/example/testgit/TestGitApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestGitApplication {
 
     public static void main(String[] args) {
-        System.out.println("Hello!!");
+        System.out.println("Hello!!!");
     }
 
 }
